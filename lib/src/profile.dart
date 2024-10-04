43e0e7e169cc4628111b2caa6b22f59c8513a4a1
Liza <urvanova02@mail.ru>
2024-10-04 21:49:58 +0300

    added separated list

--- lib/src/profile.dart
+++ lib/src/profile.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  List<String> items = ['Пол: Женский', 'Настроение: Отличное'];
+  int _keyCounter = 0;
+
+  void _addNewItem() {
+    setState(() {
+      items.add('Новый элемент: Значение ${_keyCounter++}');
+    });
+  }
+
+  void _removeItem(int index) {
+    setState(() {
+      if (index >= 0 && index < items.length) {
+        items.removeAt(index);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Профиль'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _addNewItem,
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -31,45 +59,24 @@ class ProfileScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 24),
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Пол
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Пол:',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        Text(
-                          'Женский',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ],
+            Expanded(
+              child: ListView.separated(
+                itemCount: items.length,
+                separatorBuilder: (BuildContext context, int index) =>
+                const Divider(color: Colors.grey, thickness: 1),
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    key: ValueKey(items[index]),
+                    title: Text(
+                      items[index],
+                      style: const TextStyle(fontSize: 18),
                     ),
-                    Divider(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Настроение:',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        Text(
-                          'Отличное',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ],
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () => _removeItem(index),
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
             ),
             const SizedBox(height: 24),
@@ -83,6 +90,11 @@ class ProfileScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addNewItem,
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.add),
       ),
     );
   }
